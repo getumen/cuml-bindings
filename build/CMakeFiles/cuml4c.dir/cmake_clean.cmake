bca45af3e96332fa2846ef25a0d3ad68f16da374
getumen <n.yoshihiro.jp@gmail.com>
2023-08-21 16:17:56 +0900

    remove build

--- build/CMakeFiles/cuml4c.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcuml4c.pdb"
-  "libcuml4c.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CUDA)
-  include(CMakeFiles/cuml4c.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/cuml4c.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for cuml4c.
-# This may be replaced when dependencies are built.
